00b4761ef7744ed2f9ac8ac6da834d3e5f594351
Ash Duck <Ash@Ashs-MacBook-Air.local>
2023-03-03 14:16:44 +0000

    Added variable deck support
    Every method now supports a deck of variable size, which is useful mainly for shuffling the deck after cards have been dealt

--- CMP1903M A01 2223/Pack.cs
+++ CMP1903M A01 2223/Pack.cs
@@ -12,7 +12,7 @@ namespace CMP1903M_A01_2223
 
         public Pack()
         {
-            //Initialise the card pack here
+            //Initialise the card pack here - 52 cards
             for (int suit = 1; suit <= 4; suit++)
             {
                 for (int value = 1; value <= 13; value++)
@@ -41,14 +41,19 @@ namespace CMP1903M_A01_2223
             /// 
             /// Any parameter outside 1-3 will return false for error handling
             /// </summary>
-            
+
             //Shuffles the pack based on the type of shuffle
+
+            // Keeping track of the deck size allows the deck to be shuffled after cards have been dealt
+            int deck_size = pack.Count;
+
             if (typeOfShuffle == 1)
             {
                 // Fisher-Yates Shuffle
                 // This algorithm is derived from Durstenfeld's [1964]
+                // Ref: https://dl.acm.org/doi/pdf/10.1145/364520.364540#.pdf via Wikipedia
 
-                for (int i = 51; i >= 1; i--)
+                for (int i = deck_size - 1; i >= 1; i--)
                 {
                     int j = (int)Math.Round(new Random().NextDouble() * i);
                     swap_cards(i, j);
@@ -61,17 +66,25 @@ namespace CMP1903M_A01_2223
             {
                 // Riffle Shuffle
 
+                int half_count = deck_size / 2;
+
                 // First the deck must be split into two
-                Card[] halfA = pack.GetRange(0, 26).ToArray();
-                Card[] halfB = pack.GetRange(26, 26).ToArray();
+                Card[] halfA = pack.GetRange(0, half_count).ToArray();
+                Card[] halfB = pack.GetRange(half_count, deck_size - half_count).ToArray();
                 List<Card> shuffled_deck = new List<Card>();
 
                 // Simulate the 'riffle'
 
-                for (int i = 0; i < 26; i++)                // As both sides will have 26 cards each, a half-deck for loop can be used.
+                for (int i = 0; i < half_count; i++)                // As both sides will have 26 cards each, a half-deck for loop can be used.
                 {
                     shuffled_deck.Add(halfA[i]);
                     shuffled_deck.Add(halfB[i]);
+                }
+
+                // If the halfs aren't even then there will always be one extra card in halfB that still needs to be added.
+                if (halfB.Count() > half_count)
+                {
+                    shuffled_deck.Add(halfB[halfB.Count() - 1]);
                 }
 
                 pack = shuffled_deck;                   // Set the deck to be the new shuffled pack.
@@ -94,21 +107,25 @@ namespace CMP1903M_A01_2223
         }
         public static Card deal()
         {
-            return pack[0];
+            Card dealt_card = pack[0];
+            pack.RemoveAt(0);
+            return dealt_card;
             //Deals one card
 
         }
         public static List<Card> dealCard(int amount)
         {
-            return pack.GetRange(0, amount);
+            List<Card> card_range = pack.GetRange(0, amount);
+            pack.RemoveRange(0, amount);
+            return card_range;
             //Deals the number of cards specified by 'amount'
         }
 
-        public static string ToString()
+        public static string ToString()                     // This returns the values of the deck array rather than the overarching deck object
         {
-            string[] card_strs = new string[52];
+            string[] card_strs = new string[pack.Count];
 
-            for (int i = 0; i < 52; i++)
+            for (int i = 0; i < pack.Count; i++)
             {
                 card_strs[i] = "Card(" + pack[i].Suit.ToString() + ", " + pack[i].Value.ToString() + ")";
             }
